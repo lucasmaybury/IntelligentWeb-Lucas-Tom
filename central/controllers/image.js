050e7e347d31e958eeb57e9c6affdac05daa3e9e
Lucas Maybury <lmaybury1@sheffield.ac.uk>
2021-03-26 17:13:28 +0000

    update routes and add image controller

---
+++ central/controllers/image.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+exports.sendImageToClient = function (req, res) {
+    let headers = {
+        method: 'Get',
+        headers: {'Content-Type': 'application/json'},
+        userAgent: 'localhost:3000'
+    }
+
+    fetch(`http://localhost:3001/${req.params.name}`, headers)
+        .then(res => {
+            res.json()
+        } )
+        .catch(err => {
+            if (err.status === 404) {
+                res.sendStatus(404)
+            } else {
+                res.render("error", {error: err, message: err})
+            }
+        });
+}
+
+exports.saveImageToDB = function (req, res) {
+
+}
--- central/routes/index.js
+++ central/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var image = require('../controllers/image');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
+
+router.get('/image/:name', image.sendImageToClient)
+
+router.post('/image', image.saveImageToDB);
+
 
 module.exports = router;
