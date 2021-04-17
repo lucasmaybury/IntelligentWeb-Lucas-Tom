ad524c295bec205fa4f75ca84f430c1383585048
Tom <teso1@sheffield.ac.uk>
2021-04-17 21:04:20 +0100

    Implemented joining room and welcome message

--- central/public/javascripts/index.js
+++ central/public/javascripts/index.js
@@ -1,7 +1,8 @@
 let name = null;
 let roomNo = null;
 let socket=null;
-
+let chat = io.connect('/chat');
+let news = io.connect('/news');
 
 /**
  * called by <body onload>
@@ -13,6 +14,8 @@ function init() {
     document.getElementById('initial_form').style.display = 'block';
     document.getElementById('chat_interface').style.display = 'none';
 
+    initChatSocket();
+    initNewsSocket();
     //@todo here is where you should initialise the socket operations as described in teh lectures (room joining, chat message receipt etc.)
 }
 
@@ -26,13 +29,60 @@ function generateRoom() {
     document.getElementById('roomNo').value = 'R' + roomNo;
 }
 
+
+function initChatSocket() {
+    // called when someone joins the room. If it is someone else it notifies the joining of the room
+    chat.on('joined', function (room, userId) {
+        if (userId === name) {
+            // it enters the chat
+            hideLoginInterface(room, userId);
+        } else {
+            // notifies that someone has joined the room
+            writeOnChatHistory('<b>' + userId + '</b>' + ' joined room ' + room);
+        }
+    });
+    // called when a message is received
+    chat.on('chat', function (room, userId, chatText) {
+        let who = userId
+        if (userId === name) who = 'Me';
+        writeOnChatHistory('<b>' + who + ':</b> ' + chatText);
+    });
+
+}
+
+/**
+ * it initialises the socket for /news
+ */
+function initNewsSocket(){
+    news.on('joined', function (room, userId) {
+        if (userId !== name) {
+            // notifies that someone has joined the room
+            writeOnNewsHistory('<b>'+userId+'</b>' + ' joined news room ' + room);
+        }
+    });
+
+    // called when some news is received (note: only news received by others are received)
+    news.on('news', function (room, userId, newsText) {
+        writeOnNewsHistory('<b>' + userId + ':</b> ' + newsText);
+    });
+}
+
 /**
  * called when the Send button is pressed. It gets the text to send from the interface
  * and sends the message via  socket
  */
 function sendChatText() {
     let chatText = document.getElementById('chat_input').value;
-    // @todo send the chat message
+    chat.emit('chat', roomNo, name, chatText);
+}
+/**
+ * called when the Send button is pressed for news. It gets the text to send from the interface
+ * and sends the message via  socket
+ */
+function sendNewsText (){
+    let newsText = document.getElementById('news_input').value;
+    news.emit('news', roomNo, name, newsText);
+    document.getElementById('news_input').value='';
 }
 
 /**
@@ -45,23 +95,36 @@ function connectToRoom() {
     let imageUrl= document.getElementById('image_url').value;
     if (!name) name = 'Unknown-' + Math.random();
     //@todo join the room
-    initCanvas(socket, imageUrl);
+    //initCanvas(socket, imageUrl);
     hideLoginInterface(roomNo, name);
+    chat.emit('create or join', roomNo, name);
+    news.emit('create or join', roomNo, name);
 }
 
+/**
+ * it appends the given html text to the history div
+ * @param text: teh text to append
+ */
+function writeOnChatHistory(text) {
+    let history = document.getElementById('chat_history');
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = text;
+    history.appendChild(paragraph);
+    document.getElementById('chat_input').value = '';
+}
 /**
  * it appends the given html text to the history div
  * this is to be called when the socket receives the chat message (socket.on ('message'...)
  * @param text: the text to append
  */
-function writeOnHistory(text) {
+function writeOnNewsHistory(text){
     if (text==='') return;
     let history = document.getElementById('history');
     let paragraph = document.createElement('p');
     paragraph.innerHTML = text;
     history.appendChild(paragraph);
     // scroll to the last element
-    history.scrollTop = history.scrollHeight;
+    //history.scrollTop = history.scrollHeight;
     document.getElementById('chat_input').value = '';
 }
 
--- central/socket.io/socket-io.js
+++ central/socket.io/socket-io.js
@@ -1,9 +1,55 @@
-
 exports.init = function(io) {
-    io.sockets.on('connection', function (socket) {
+
+    /**
+     * The message namespace for handling messages between clients
+     */
+    const messages = io.of("/messages").on("connection", (socket) => {
         try {
-            // insert here your event
-        } catch (e) {
-        }
+            // Enroll in a room
+            socket.on("enrol", (room, user) => {
+                socket.join(room);
+                messages.to(room).emit("joined", user);
+            });
+            // For actually sending message
+            socket.on("chat", (room, user, message) => {
+                messages.to(room).emit("chat", user, message);
+            });
+e
+            // To show typing animation like on messenger
+            socket.on("typing", (room, user) => {
+                messages.to(room).emit("typing", room, user);
+            })
+
+            // On disconnect
+            socket.on("disconnect", () => {
+                console.log("User Disconnected")
+            });
+
+        } catch (e) {}
     });
+
+    // the news namespace
+    const news= io
+        .of('/news')
+        .on('connection', function (socket) {
+            try {
+                /**
+                 * it creates or joins a room
+                 */
+                socket.on('create or join', function (room, userId) {
+                    socket.join(room);
+                    socket.broadcast.to(room).emit('joined', room, userId);
+                });
+
+                socket.on('news', function (room, userId, chatText) {
+                    socket.broadcast.to(room).emit('news', room, userId, chatText);
+                });
+
+                socket.on('disconnect', function(){
+                    console.log('someone disconnected');
+                });
+            } catch (e) {
+            }
+        });
+
 }
