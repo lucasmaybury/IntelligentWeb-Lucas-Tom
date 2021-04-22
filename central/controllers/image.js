0fc4390f1c37ce15388a311a73ff87d477969427
Tom <teso1@sheffield.ac.uk>
2021-04-22 17:06:27 +0100

    Merge and Revert

--- central/controllers/image.js
+++ central/controllers/image.js
@@ -40,14 +40,15 @@ exports.saveImageToDB = function (req, res) {
         userAgent: 'localhost:3000',
         body: JSON.stringify(req.body) //pass body through to database server from client
     }
-
+    console.log(req.body)
     fetch(`http://localhost:3001`, headers)//make the request
         .then(response => {
             if(response.status !== 201) { throw new Error('error saving to database') } //forces program into catch block below
+            console.log('saving image')
             res.status(201).end();
         })
         .catch(err => {
-            console.log(err);
+            console.error(err);
             res.status(500).render("error", {error: err, message: err});
         });
 }
