efd3b855e8fee629592ebde1a87c70e61f89453b
Tom <teso1@sheffield.ac.uk>
2021-05-18 20:11:03 +0100

    Camera Opening

--- central/public/javascripts/capture.js
+++ central/public/javascripts/capture.js
@@ -1,11 +1,34 @@
-function startup(){
-
+async function camera(){
     navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
+        video: true,
+        height: true
     }).then(stream => {
         document.getElementById("video").srcObject = stream;
     }).catch(console.error)
-}
 
-//window.addEventListener("load", startup, false);
+}
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.canvas.getContext('2d');
+var localMediaStream = null;
+video.addEventListener('snap', snapshot, false);
+navigator.getUserMedia({video: true}, function (stream) {
+    video.src = window.URL.createObjectURL(stream);
+    localMediaStream = stream;
+});
+
+function snapshot(){
+    if(localMediaStream){
+        ctx.drawImage(video,0,0, 640, 480);
+        document.querySelector('img').src
+        = canvas.toDataURL('image/png');
+    }
+    console.log("successful");
+}
+/*
+
+document.getElementById('snap'.addEventListener('click', () => {
+        ctx.drawImage(video, 0, 0, 640, 480);
+        console.log("sick");
+    })
+)*/
