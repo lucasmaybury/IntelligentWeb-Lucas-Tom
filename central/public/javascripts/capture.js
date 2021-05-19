c769e69737cf3493806c9234ac168700cc25cfc9
Tom <teso1@sheffield.ac.uk>
2021-05-19 13:21:02 +0100

    Camera takes photo and displays image

--- central/public/javascripts/capture.js
+++ central/public/javascripts/capture.js
@@ -1,3 +1,5 @@
+/*
+//function that opens the camera
 async function camera(){
     navigator.mediaDevices.getUserMedia({
         video: true,
@@ -7,28 +9,133 @@ async function camera(){
     }).catch(console.error)
 
 }
+
+//setting variables for canvas lay over and
+//action listener for the snapshot function
+
 var video = document.querySelector('video');
 var canvas = document.querySelector('canvas');
-var ctx = canvas.canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
+
 var localMediaStream = null;
-video.addEventListener('snap', snapshot, false);
+video.addEventListener('click', snapshot, false);
+
 navigator.getUserMedia({video: true}, function (stream) {
     video.src = window.URL.createObjectURL(stream);
     localMediaStream = stream;
 });
 
+// function for snapshot
+
 function snapshot(){
     if(localMediaStream){
-        ctx.drawImage(video,0,0, 640, 480);
-        document.querySelector('img').src
-        = canvas.toDataURL('image/png');
+        ctx.drawImage(video,0,0);
+        document.querySelector('img').src = canvas.toDataURL('image/png');
     }
-    console.log("successful");
+    alert('done');
 }
-/*
 
-document.getElementById('snap'.addEventListener('click', () => {
-        ctx.drawImage(video, 0, 0, 640, 480);
-        console.log("sick");
-    })
-)*/
+ */
+
+(function openCamera() {
+    // The width and height of the captured photo. We will set the
+    // width to the value defined here, but the height will be
+    // calculated based on the aspect ratio of the input stream.
+
+    var width = 320;    // We will scale the photo width to this
+    var height = 0;     // This will be computed based on the input stream
+
+    // |streaming| indicates whether or not we're currently streaming
+    // video from the camera. Obviously, we start at false.
+
+    var streaming = false;
+
+    // The various HTML elements we need to configure or control. These
+    // will be set by the startup() function.
+
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var start = null;
+
+    function startup() {
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        photo = document.getElementById('photo');
+        start = document.getElementById('start');
+
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(err) {
+                console.log("An error occurred: " + err);
+            });
+
+        video.addEventListener('canplay', function(ev){
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth/width);
+
+                // Firefox currently has a bug where the height can't be read from
+                // the video, so we will make assumptions if this happens.
+
+                if (isNaN(height)) {
+                    height = width / (4/3);
+                }
+
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
+
+        start.addEventListener('click', function(ev){
+            takepicture();
+            ev.preventDefault();
+        }, false);
+
+        clear();
+    }
+
+    // Fill the photo with an indication that none has been
+    // captured.
+
+    function clear() {
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+    }
+
+    // Capture a photo by fetching the current contents of the video
+    // and drawing it into a canvas, then converting that to a PNG
+    // format data URL. By drawing it on an offscreen canvas and then
+    // drawing that to the screen, we can change its size and/or apply
+    // other changes before drawing it.
+
+    function takepicture() {
+        var context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            var data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+        } else {
+            clear();
+        }
+    }
+
+    // Set up our event listener to run the startup process
+    // once loading is complete.
+    window.addEventListener('load', startup, false);
+})();
+
+
+
